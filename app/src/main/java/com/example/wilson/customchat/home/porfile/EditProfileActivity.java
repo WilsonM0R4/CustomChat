cfb76fd7e8b4fd8a518322b9780dbe73a29bc8c5
Wilson Mora <wilsonfermora19.97@gmail.com>
2016-08-31 11:21:24 -0500

    profile image improvement

--- app/src/main/java/com/example/wilson/customchat/home/porfile/EditProfileActivity.java
+++ app/src/main/java/com/example/wilson/customchat/home/porfile/EditProfileActivity.java
@@ -4,10 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,7 +17,6 @@ import android.widget.Toast;
 import com.example.wilson.customchat.R;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
@@ -31,12 +28,16 @@ import butterknife.OnClick;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    @Bind(R.id.toolAppbar) Toolbar appBar;
-    @Bind(R.id.editCancel) ImageButton cancelButton;
-    @Bind(R.id.editSave) ImageButton saveButton;
-    @Bind(R.id.editProfileImage) ImageView editProfileImage;
+    @Bind(R.id.toolAppbar)
+    Toolbar appBar;
+    @Bind(R.id.editCancel)
+    ImageButton cancelButton;
+    @Bind(R.id.editSave)
+    ImageButton saveButton;
+    @Bind(R.id.editProfileImage)
+    ImageView editProfileImage;
 
-    private static final int REQUEST_CAPTURE = 1;
+    private static final int REQUEST_CAPTURE = 0;
     private String newProfileImagePath;
     File profileImageFile;
 
@@ -51,95 +52,72 @@ public class EditProfileActivity extends AppCompatActivity {
 
     }
 
-    /*if(image!=null){
-        editProfileImage.setImageBitmap(imageFromfile(profileImageFile));
-    }else{
-        Log.e("Profile image","no image to load");
-    }*/
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
         Bitmap imageBitmap;
 
-        if(requestCode == REQUEST_CAPTURE && resultCode == Activity.RESULT_OK){
-            try {
-                InputStream imageStream = getContentResolver().openInputStream(data.getData());
+        if (requestCode == REQUEST_CAPTURE && resultCode == Activity.RESULT_OK) {
 
-                imageBitmap = BitmapFactory.decodeStream(imageStream);
+            imageBitmap = (Bitmap) data.getExtras().get("data");
 
-                imageStream.close();
-
-                if(imageBitmap!=null){
-                    editProfileImage.setImageBitmap(imageBitmap);
-                }
-
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (imageBitmap != null) {
+                editProfileImage.setImageBitmap(imageBitmap);
+            } else {
+                Log.e("activity result", "bitmap is null");
             }
         }
-
-        super.onActivityResult(requestCode,resultCode,data);
 
     }
 
     @OnClick(R.id.editSave)
-    protected void sendUserDataToCloud(){
-        Toast.makeText(this,"run to the hiiiiiiiils!!!",Toast.LENGTH_SHORT).show();
+    protected void sendUserDataToCloud() {
+        Toast.makeText(this, "run to the hiiiiiiiils!!!", Toast.LENGTH_SHORT).show();
     }
 
     @OnClick(R.id.editCancel)
-    protected void cancel(){
+    protected void cancel() {
         finish();
     }
 
     @OnClick(R.id.editProfileImage)
-    protected void editImage(){
+    protected void editImage() {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        if(cameraIntent.resolveActivity(getPackageManager())!=null){
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
 
-            try{
+            try {
                 profileImageFile = createImageFile();
-            }catch (IOException ex){
+            } catch (IOException ex) {
                 ex.printStackTrace();
             }
 
-            if(profileImageFile != null){
-                //Uri imageUri = FileProvider.getUriForFile(this,"com.example.wilson.customchat",profileImageFile);
-
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,profileImageFile.getAbsolutePath());
-                startActivityForResult(cameraIntent,REQUEST_CAPTURE);
+            if (profileImageFile != null) {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, profileImageFile.getAbsolutePath());
+                startActivityForResult(cameraIntent, REQUEST_CAPTURE);
             }
 
         }
 
     }
 
-    private File createImageFile() throws IOException{
+    private File createImageFile() throws IOException {
 
         String imageName = "CChat_profile_img_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageType = ".jpg";
 
         File storagePath = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageName,imageType,storagePath);
+        File image = File.createTempFile(imageName, imageType, storagePath);
 
         newProfileImagePath = image.getAbsolutePath();
 
         return image;
     }
 
-    private Bitmap imageFromfile(File imageFile){
+    private Bitmap imageFromFile(File imageFile) {
 
-        Bitmap image = null;
-
-        if(imageFile!=null){
-            image = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-        }
-        return image;
+        return BitmapFactory.decodeFile(imageFile.getAbsolutePath());
     }
 
 
