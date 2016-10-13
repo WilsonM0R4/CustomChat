47bd7d63d1c3f8c6844c72d875725054d7ba7363
Wilson Mora <wilsonfermora19.97@gmail.com>
2016-10-12 19:27:11 -0500

    Complete contacts work

---
+++ app/src/main/java/com/example/wilson/customchat/home/chats/Message.java
@@ -0,0 +1,44 @@
+package com.example.wilson.customchat.home.chats;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wmora on 10/12/16.
+ */
+
+public class Message {
+
+    private String date;
+    private  String hour;
+    private  ArrayList<String> users;
+    private  String deliver;
+    private  String content;
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public String getHour(){
+        return hour;
+    }
+
+    public void setHour(String hour){
+        this.hour = hour;
+    }
+
+    public ArrayList<String> getUsers(){
+        return users;
+    }
+
+    public void setUsers(ArrayList<String> users){
+        this.users = users;
+    }
+
+    /*public String getDeliver(){
+
+    }*/
+}
